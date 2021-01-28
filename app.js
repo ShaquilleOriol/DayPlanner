8f1916761002dc8cc43c520f85f569fc39b15f94
Shaquille Oriol <shaquille.oriol@gmail.com>
2021-01-27 23:03:58 -0600

    unworking code

--- app.js
+++ app.js
@@ -1,52 +1,87 @@
 $(document).ready(function () {
   $("#presentDay").text(moment());
-  $("saveBtn").on("click", function () {
-
-    var input = $(this).siblings(description).val();
-    console.log(input)
-    var time = $(this).parent().attr(id);
-    console.log(time)
-
-    localStorage.setItem(time, input)
+  $(".saveBtn9").on("click", function () {
+    console.log("hi");
+    var input9 = $(".description9").val();
+    var time9 = $(".hour").text();
+    localStorage.setItem("time9", input9);
   });
-console.log("hi")
-  function colorOverlay() {
 
-    var presentTime = moment().hours();
+  $(".saveBtn1").on("click", function () {
+    var input1 = $(".description1").val();
+    var time1 = $(".hour").text();
+    localStorage.setItem("time1", input1);
+  });
+  $(".saveBtn2").on("click", function () {
+    var input2 = $(".description2").val();
+    var time2 = $(".hour").text();
+    localStorage.setItem("time2", input2);
+  });
+  $(".saveBtn3").on("click", function () {
+    var input3 = $(".description3").val();
+    var time3 = $(".hour").text();
+    localStorage.setItem("time3", input3);
+  });
+  $(".saveBtn4").on("click", function () {
+    var input4 = $(".description4").val();
+    var time4 = $(".hour").text();
+    localStorage.setItem("time4", input4);
+  });
+  $(".saveBtn5").on("click", function () {
+    var input5 = $(".description5").val();
+    var time5 = $(".hour").text();
+    localStorage.setItem("time5", input5);
+  });
+  $(".saveBtn6").on("click", function () {
+    var input6 = $(".description6").val();
+    var time6 = $(".hour").text();
+    localStorage.setItem("time6", input6);
+  });
+  $(".saveBtn7").on("click", function () {
+    var input7 = $(".description7").val();
+    var time7 = $(".hour").text();
+    localStorage.setItem("time7", input7);
+  });
+  $(".saveBtn8").on("click", function () {
+    var input8 = $(".description8").val();
+    var time8 = $(".hour").text();
+    localStorage.setItem("time8", input8);
+  });
 
-    $(time - block).each(function () {
-      var newTime = parseInt($(this).attr(id).split("-")[1]);
+  // function colorOverlay() {
+  //   var presentTime = moment().hours();
 
-      if (newTime < presentTime) {
-        $(this).addClass("past");
-      }
-      else if (newTime === presentTime) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-      }
-      else {
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-        $(this).addClass("future");
-      }
-    })
-    $("#9-hour .description").val(localStorage.getItem('9-hour'));
+  //   $(".time-block").each(function () {
+  //     var newTime = parseInt($(this).attr(id).split("-")[1]);
 
-    $("#10-hour .description").val(localStorage.getItem('10-hour'));
+  //     if (newTime < presentTime) {
+  //       $(this).addClass("past");
+  //     } else if (newTime === presentTime) {
+  //       $(this).removeClass("past");
+  //       $(this).addClass("present");
+  //     } else {
+  //       $(this).removeClass("past");
+  //       $(this).removeClass("present");
+  //       $(this).addClass("future");
+  //     }
+  //   });
+  // }
+  $("#9-hour").val(localStorage.getItem("time1"));
 
-    $("#11-hour .description").val(localStorage.getItem('11-hour'));
+  $("#10-hour").val(localStorage.getItem("time2"));
 
-    $("#12-hour .description").val(localStorage.getItem('12-hour'));
+  $("#11-hour").val(localStorage.getItem("time3"));
 
-    $("#13-hour .description").val(localStorage.getItem('13-hour'));
+  $("#12-hour").val(localStorage.getItem("time4"));
 
-    $("#14-hour .description").val(localStorage.getItem('14-hour'));
+  $("#13-hour").val(localStorage.getItem("time5"));
 
-    $("#15-hour .description").val(localStorage.getItem('15-hour'));
+  $("#14-hour").val(localStorage.getItem("time6"));
 
-    $("#16-hour .description").val(localStorage.getItem('16-hour'));
+  $("#15-hour").val(localStorage.getItem("time7"));
 
-    $("#17-hour .description").val(localStorage.getItem('17-hour'));
-    colorOverlay();
-  }
-})
+  $("#16-hour").val(localStorage.getItem("time8"));
+
+  $("#17-hour").val(localStorage.getItem("time9"));
+  // colorOverlay();
+});
